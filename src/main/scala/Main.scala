d523134be2a5b29552218585e79a018cd5dc7e86
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-03-01 21:43:41 +0100

    upd main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,52 +1,69 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import ranking.{PageRank, RankingAlgorithm}
-import utils.{FileUtils, SparkContextSingleton, VisualizationUtils}
+import org.apache.spark.graphx.{Edge, Graph}
+import ranking.{DistributedPageRank, PageRank, PageRankLibrary, RankingAlgorithm}
+import utils.{FileUtility, SparkContextSingleton, VisualizationUtils}
 
 
 object Main {
 
-    def performRanking(edgesList: List[(Int, Int)], N: Int, algorithm: RankingAlgorithm): List[(Int, Float)] = {
+    def performRanking(graphFilePath: String, edgesList: List[(Int, Int)], N: Int, algorithm: RankingAlgorithm): List[(Int, Float)] = {
         algorithm match {
-            //case r: InDegreeRank => r.rank(edgesList, N)
-            case r: PageRank =>
+            case r: PageRank => r.rank(edgesList, N)
+            case r: DistributedPageRank =>
                 val sc = SparkContextSingleton.getContext
                 val distEdgesList = sc.parallelize(edgesList)
                 r.setContext(sc)
                 r.rank(distEdgesList, N)
+            case r: PageRankLibrary =>
+                val sc = SparkContextSingleton.getContext
+                r.setContext(sc)
+
+                val nodes = FileUtility.loadNodesFromFile(graphFilePath).toSeq
+                val vertexMap = (0 until nodes.size).map(i => nodes(i) -> nodes(i)._1.toLong).toMap
+                vertexMap.foreach(m => (println(m._1), println(m._2)))
+                val edgeList = FileUtility.loadGraphFromFile(graphFilePath).map(x => Edge((x._1),(x._2),"")).toSeq
+
+                val distEdgesList = sc.parallelize(edgeList.toSeq)
+                val distNodes = sc.parallelize(vertexMap.toSeq.map(_.swap))
+
+                val graph = Graph(distNodes,distEdgesList)
+                r.rank(graph, N)
 
         }
     }
 
     def main(args: Array[String]): Unit = {
         // Parse program arguments
-        val graphFilePath = if (args.length > 0) args(0) else "data/citations_500.txt"
-        val algorithmName = if (args.length > 1) args(1) else "PageRank"
+        val graphFilePath = "data/citations_500.txt"
+        val algorithmName = if (args.length > 1) args(1) else "PageRankLibrary"
         // PageRank tolerance
         val prTolerance: Float = 0.000000001f
         // Output parameters
         val topK: Int = 3
         //val outputFilename: String = "result_%s.html".format(algorithmName)
         // Pick the ranking algorithm
-        val r : RankingAlgorithm = algorithmName match {
-            //case "InDegreeRank" => new InDegreeRank
-            case "PageRank" => new PageRank(tolerance = prTolerance)
-        }
+        val r : RankingAlgorithm =
+            algorithmName match {
+                case "DistributedPageRank" => new DistributedPageRank()
+                case "PageRank" => new PageRank(tolerance = prTolerance)
+                case "PageRankLibrary" => new PageRankLibrary()
+                }
+
         // Report algorithm
         println("Using algorithm "+algorithmName);
         // Load data
         println("Loading graph from "+graphFilePath);
-        val edgesList = FileUtils.loadGraphFromFile(graphFilePath)
-        val nodes = FileUtils.loadNodesFromFile(graphFilePath)
+        val edgesList = FileUtility.loadGraphFromFile(graphFilePath)
+        val nodes = FileUtility.loadNodesFromFile(graphFilePath)
         val N: Int = nodes.size
         // Display graph data
         println("Loaded "+N+" nodes.")
         println("Loaded "+edgesList.size+" edges.")
         // Perform ranking
-        val ranking = performRanking(edgesList, N, r)
+        val ranking = performRanking(graphFilePath, edgesList, N, r)
         // Print all the results
         VisualizationUtils.printTopK(ranking, nodes, k = topK)
-        // Output results to a html page
-        //VisualizationUtils.outputHtmlPage(outputFilename, graphFilePath, ranking, nodes, k = topK)
-        //println("Saved results in "+outputFilename);
+
     }
+
+
 }
