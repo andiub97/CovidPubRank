7d0af6c5b84e9f7cb8e6f916b17cb98b256ee118
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-03-10 16:58:25 +0100

    comments removed

--- src/main/scala/ranking/PageRank.scala
+++ src/main/scala/ranking/PageRank.scala
@@ -24,29 +24,7 @@ class PageRank(val tolerance: Float) extends RankingAlgorithm {
       val damping: Float = 0.85f
 
       // Runs PageRank until convergence.
-      /*if (tolerance > 0f) {
-        var oldPr: Map[Int, Float] = (0 until N).map(nodeIndex => (nodeIndex, 0f)).toMap
-        var maxDiff: Float = 10f
 
-        do {
-          oldPr = pr
-          pr = pr.map { case (nodeId: Int, nodePr: Float) =>
-            (nodeId, (1 - damping) / N + damping *
-              edgesList.filter { case (incoming: Int, dest: Int) => dest == nodeId }
-                .map { case (incoming: Int, dest: Int) => pr(incoming) / outgoingCnt(incoming) }.sum
-            )
-          }
-
-          maxDiff = pr.map { case (nodeId: Int, nodePr: Float) =>
-            (nodeId, abs(nodePr - oldPr(nodeId)))
-          }.maxBy(_._2)._2
-
-        } while (maxDiff > tolerance)
-
-      }
-
-      //Runs PageRank for a fixed number of iterations.
-      else {*/
         for (t <- 1 to maxIter) {
           // pr => (nodeId, pr(nodeId))
           pr = pr.map { case (nodeId: Int, nodePr: Float) =>
@@ -55,10 +33,7 @@ class PageRank(val tolerance: Float) extends RankingAlgorithm {
                 .map { case (incoming: Int, dest: Int) => pr(incoming) / outgoingCnt(incoming) }.sum
             )
           }
-        //}
       }
-
-
       // sort in descending order by PageRank value
       pr.toList.sortBy(- _._2)
     }
