53390ed8a66f95a0df07865c0bdb0d9bfcdd32e7
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-03-21 20:12:33 +0100

    add export algorithm's execution time

--- src/main/scala/utils/FileUtility.scala
+++ src/main/scala/utils/FileUtility.scala
@@ -2,19 +2,21 @@ package utils
 
 import org.apache.spark.rdd.RDD
 
+import java.io.{FileWriter, PrintWriter}
+
+
 object FileUtility {
 
     /**
      * Loads a graph's list of edges from a given file path.
      * */
-    def loadGraphFromFile(path: String): List[(Int, Int)] = {
+    def loadGraphFromFile(path: String): RDD[(Int, Int)] = {
         val graphFile = SparkContextSingleton.getContext.textFile(path)
-        val edgesList: List[(Int, Int)] = graphFile
+        val edgesList: RDD[(Int, Int)] = graphFile
           .filter(line => line.startsWith("e"))
           .map(line => line.split("\\s"))
           .map(tokens => (tokens(1).toInt, tokens(2).toInt))
-          .collect().toList
-        edgesList
+          edgesList
     }
 
     /**
@@ -28,6 +30,15 @@ object FileUtility {
           .map(tokens => (tokens._1(1).toInt, tokens._2.mkString(" ")))
           .collect.toMap[Int, String]
         nodes
+    }
+
+    def exportToTxt(path: String, exec_times: Map[String, Double]): Unit = {
+        //SparkContextSingleton.getContext.parallelize(exec_times.toSeq).saveAsTextFile(path)
+        val pw = new PrintWriter(new FileWriter(path))
+        pw.println("algorithmName,exec_time")
+        exec_times.foreach(e => (pw.print(e._1 + ","), pw.println(e._2)))
+        pw.close()
+
     }
 
 }
