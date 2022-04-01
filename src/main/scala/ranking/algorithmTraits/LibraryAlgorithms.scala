09823cc7a3f5841b76384e6ca44e8e68ff2c840f
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-04-01 16:40:55 +0200

    add ranking algorithms traits

--- src/main/scala/ranking/algorithmTraits/LibraryAlgorithms.scala
+++
@@ -1,9 +0,0 @@
-package ranking.algorithmTraits
-
-import org.apache.spark.graphx.Graph
-
-trait LibraryAlgorithms extends AlgorithmInterface {
-
-  type T = Graph[(Int, String), String]
-  override def rank(edgesList: T, N: Int): List[(Int, Float)]
-}
--- src/main/scala/ranking/algorithmTraits/NotLibraryAlgorithms.scala
+++
@@ -1,9 +0,0 @@
-package ranking.algorithmTraits
-
-import org.apache.spark.rdd.RDD
-
-trait NotLibraryAlgorithms extends AlgorithmInterface {
-
-  type T = RDD[(Int, Int)]
-  def rank(edgesList: T, N: Int): List[(Int, Float)]
-}
