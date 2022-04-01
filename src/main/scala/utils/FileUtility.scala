36b2862f82b3a6ed5ae03b73e3dc5be7f96c823c
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-04-01 16:47:19 +0200

    final FileUtility.scala

---
+++ src/main/scala/utils/FileUtility.scala
@@ -0,0 +1,44 @@
+package utils
+
+import java.io.{FileWriter, PrintWriter}
+
+
+object FileUtility {
+
+    def exportAlgorithmsResults(path: String, exec_times: Map[String, (Double, String)]): Unit = {
+        //SparkContextSingleton.getContext.parallelize(exec_times.toSeq).coalesce(1,true).saveAsTextFile(path)
+        val pw = new PrintWriter(new FileWriter(path))
+        pw.println("algorithmName,exec_time,citation")
+        exec_times.foreach(e => (pw.print(e._1 + ","), pw.print(e._2._1 + ","), pw.println(e._2._2)))
+        pw.close()
+    }
+
+
+
+
+    /**
+     * Loads a graph's list of edges from a given file path.
+     * */
+    def loadGraphFromFile(path: String): List[(Int, Int)] = {
+        val graphFile = SparkContextSingleton.getContext.textFile(path)
+        val edgesList: List[(Int, Int)] = graphFile
+          .filter(line => line.startsWith("e"))
+          .map(line => line.split("\\s"))
+          .map(tokens => (tokens(1).toInt, tokens(2).toInt)).collect().toList
+          edgesList
+    }
+
+    /**
+     * Loads node labels from a given file path.
+     * */
+    def loadNodesFromFile(path: String): Map[Int, String] = {
+        val graphFile = SparkContextSingleton.getContext.textFile(path)
+        val nodes: Map[Int, String] = graphFile
+          .filter(line => line.startsWith("n"))
+          .map(line => line.split("\\s").splitAt(2))
+          .map(tokens => (tokens._1(1).toInt, tokens._2.mkString(" ")))
+          .collect.toMap[Int, String]
+        nodes
+    }
+
+}
