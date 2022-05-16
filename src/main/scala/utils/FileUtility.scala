95469459fdc15356c13c08222ed0e1904f40cfeb
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-05-16 18:28:41 +0200

    add output stats method for cloud env

--- src/main/scala/utils/FileUtility.scala
+++ src/main/scala/utils/FileUtility.scala
@@ -1,8 +1,8 @@
 package utils
 
-import org.apache.spark.{RangePartitioner, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{RangePartitioner, SparkContext}
 import ranking.algorithmTraits.AlgorithmInterface
 import ranking.{DistributedPageRank, DistributedPageRankOptimized}
 
@@ -12,28 +12,25 @@ import java.io.{FileWriter, PrintWriter}
 object FileUtility {
 
     def chooseDistributedPageRank(str: String): AlgorithmInterface = {
-        if ((str == "data/dataset_1015681.txt") || (str == "gs://datasets/dataset_1015681.txt")){
+        if ((str == "data/dataset_1015681.txt") || (str == "gs://articles_datasets/dataset_1015681.txt")){
             new DistributedPageRankOptimized()
         } else{
             new DistributedPageRank()
         }
     }
 
+    def exportAlgorithmsResults(path: String, exec_times: Map[String, Double], dataset: String): Unit = {
 
-    def exportAlgorithmsResults(path: String, exec_times: Map[String, Double], numWorker: String, dataset: String, sparkContext:SparkContext): Unit = {
 
-        if(numWorker == "local"){
-            val pw = new PrintWriter(new FileWriter(path))
-            exec_times.foreach(e => (pw.print(e._1 + ","), pw.print(e._2 + ","), pw.println(dataset)))
-            pw.close()
+        val pw = new PrintWriter(new FileWriter(path))
+        exec_times.foreach(e => (pw.print(e._1 + ","), pw.print(e._2 + ","), pw.println(dataset)))
+        pw.close()
+    }
 
-        }else {
-            if(numWorker != "local"){
-                sparkContext.parallelize(exec_times.map(t => (t._1, t._2, numWorker)).toSeq).coalesce(1,true).saveAsTextFile(path)
+    def exportAlgorithmsResultsOnCloud(path: String, exec_times: Map[String, Double], mode: String, dataset: String, workers: String, sparkContext:SparkContext): Unit = {
 
-            }else{
-                sparkContext.parallelize(exec_times.toSeq).coalesce(1,true).saveAsTextFile(path)
-            }
+        if((mode == "localOnCloud") || ( mode == "distributedOnCloud")) {
+          sparkContext.parallelize(exec_times.map(t => (t._1, t._2, dataset.substring(23), workers)).toSeq).coalesce(1, true).saveAsTextFile(path)
         }
 
 
