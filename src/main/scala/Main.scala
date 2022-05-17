3d2fd67ce10c8b5d48126fbbc8ef1b38f396b75d
andiub97 <43032657+andiub97@users.noreply.github.com>
2022-05-17 11:23:11 +0200

    final commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -23,8 +23,9 @@ object Main {
 
             case r : PageRank =>
 
+                val list = edgesList.collect().toList
                 val start_time = System.nanoTime
-                val ranking = r.rank(edgesList.collect().toList: List[(Int,Int)],N,sparkContext)
+                val ranking = r.rank(list: List[(Int,Int)],N,sparkContext)
                 val duration = (System.nanoTime - start_time) / 1e9d
                 r.getClass.getName -> (ranking, duration)
 
@@ -47,8 +48,8 @@ object Main {
 
         // Parse program arguments
         val master = args(0)
-        val algorithmName = if (args.length > 1) args(1) else "allAlgorithms"
-        val graphFilePath = if (args.length > 2) args(2) else "data/dataset_9647.txt"
+        val algorithmName = if (args.length > 1) args(1) else "PageRank"
+        val graphFilePath = if (args.length > 2) args(2) else "data/dataset_1015681.txt"
         val outputFilePath = if (args.length > 3) args(3) else "src/main/scala/output"
         val parallelism =  if (args.length > 4) args(4) else "4"
         val mode = if (args.length > 5) args(5) else "local"
